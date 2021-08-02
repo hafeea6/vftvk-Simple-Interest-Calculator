03782a5a187a9cba4b9daf82588c7a8e6731dee0
hafeea6 <88290974+hafeea6@users.noreply.github.com>
2021-08-01 20:06:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-function compute()
-{
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+function compute() {
+    //Get the values and calculate 
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+    //Create the Interest text
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
 
-    if (principal == "") { //Here Check is empty
-        alert("Amount can't by empty or alphabet character");
-        document.getElementById("principal").focus();
-        return false;
-    }else{
-        if (principal <= 0) { //Here check is a positive number
-            alert("Enter a positive number");
-            document.getElementById("principal").focus();
-            return false;
-        }
-    }
-
-
-    var HDB = new Date().getFullYear(); //Mark Anthony Villanueva - Peer Review Project Here get the year
-    var future= parseInt(HDB,10) + parseInt(years,10);
-    var interest= principal * years * rate / 100;
-    var result= "If you deposit <mark>" + principal + "</mark>,<br> at an interest rate of <mark>" + rate + "</mark> % <br> You will receive an amount of <mark>" + interest + ",</mark><br> in the year <mark>" + future + "</mark>";
-    document.getElementById("result").innerHTML=result;
 }
 
-function showVal(newVal){ //Show range value in span
-    document.getElementById("showRate").innerHTML=newVal + '%';
+//update ther ate value
+function getSliderValue() {
+    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+}
+
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
 }
